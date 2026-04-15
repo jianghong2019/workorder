6f0dc33c99d9363a11caa3309064fd568bc2662b
jianghong2019 <jianghong2019@example.com>
2026-04-15 01:05:29 +0000

    Deploy to GitHub Pages

---
+++ icons/icons.js
@@ -0,0 +1,154 @@
+export const icons = [
+  {
+    name: 'icon_allManage',
+    url: 'icon_allManage.png',
+  },
+  {
+    name: 'icon_noManage',
+    url: 'icon_noManage.png',
+  },
+  {
+    name: 'icon_noManage_bg',
+    url: 'icon_noManage_bg.png',
+  },
+  {
+    name: 'icon_popup',
+    url: 'icon_popup.png',
+  },
+  {
+    name: 'icon_order_status_0',
+    url: 'icon_order_status_0.png',
+  },
+  {
+    name: 'icon_order_status_1',
+    url: 'icon_order_status_1.png',
+  },
+  {
+    name: 'icon_order_status_2',
+    url: 'icon_order_status_2.png',
+  },
+  {
+    name: 'icon_order_status_3',
+    url: 'icon_order_status_3.png',
+  },
+  {
+    name: 'icon_order_status_4',
+    url: 'icon_order_status_4.png',
+  },
+  {
+    name: 'icon_person_status_0',
+    url: 'icon_person_status_0.png',
+  },
+  {
+    name: 'icon_person_status_1',
+    url: 'icon_person_status_1.png',
+  },
+  {
+    name: 'icon_person_status_2',
+    url: 'icon_person_status_2.png',
+  },
+  {
+    name: 'icon_person_status1',
+    url: 'icon_person_status1.png',
+  },
+  {
+    name: 'icon_person_status2',
+    url: 'icon_person_status2.png',
+  },
+  {
+    name: 'icon_person_status3',
+    url: 'icon_person_status3.png',
+  },
+  {
+    name: 'icon_person',
+    url: 'icon_person.png',
+  },
+  {
+    name: 'icon_preview',
+    url: 'preview.png',
+  },
+  {
+    name: 'icon_person_1',
+    url: 'icon_person_1.png',
+  },
+  {
+    name: 'icon_person_2',
+    url: 'icon_person_2.png',
+  },
+  {
+    name: 'icon_person_3',
+    url: 'icon_person_3.png',
+  },
+  {
+    name: 'icon_person_4',
+    url: 'icon_person_4.png',
+  },
+  {
+    name: 'icon_person_5',
+    url: 'icon_person_5.png',
+  },
+  {
+    name: 'icon_person_6',
+    url: 'icon_person_6.png',
+  },
+  {
+    name: 'icon_person_7',
+    url: 'icon_person_7.png',
+  },
+  {
+    name: 'icon_person_8',
+    url: 'icon_person_8.png',
+  },
+  {
+    name: 'icon_person_9',
+    url: 'icon_person_9.png',
+  },
+  {
+    name: 'icon_person_10',
+    url: 'icon_person_10.png',
+  },
+  {
+    name: 'icon_order_1',
+    url: 'icon_order_1.png',
+  },
+  {
+    name: 'icon_order_2',
+    url: 'icon_order_2.png',
+  },
+  {
+    name: 'icon_order_3',
+    url: 'icon_order_3.png',
+  },
+  {
+    name: 'icon_order_4',
+    url: 'icon_order_4.png',
+  },
+  {
+    name: 'icon_order_5',
+    url: 'icon_order_5.png',
+  },
+  {
+    name: 'icon_order_6',
+    url: 'icon_order_6.png',
+  },
+  {
+    name: 'icon_order_7',
+    url: 'icon_order_7.png',
+  },
+  {
+    name: 'icon_order_8',
+    url: 'icon_order_8.png',
+  },
+  {
+    name: 'icon_order_9',
+    url: 'icon_order_9.png',
+  },
+  {
+    name: 'icon_order_10',
+    url: 'icon_order_10.png',
+  },
+  {
+    name: 'icon-line-route',
+    url: 'icon-line-route.png',
+  },
+];
